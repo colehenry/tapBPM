99bf0e2d75377453af694dcfa2b7f53d392e99a8
Cole Henry <crhenry81@gmail.com>
2017-05-18 19:29:08 -0400

    hooked up text and button to main activity;

--- app/src/main/java/music/c19/tapbpm/MainActivity.java
+++ app/src/main/java/music/c19/tapbpm/MainActivity.java
@@ -2,6 +2,9 @@ package music.c19.tapbpm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final TextView mainText = (TextView)this.findViewById(R.id.mainText);
+        mainText.setText("BPM - 140");
+
+        final Button tapButton = (Button)this.findViewById(R.id.tapButton);
+        tapButton.setText("Tap Here");
+
+        tapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainText.setText("BPM - 190");
+
+            }
+        });
     }
 }
